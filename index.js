1a4ab3f263a22f88bee6b73c869afddb75cbb24f
DoubledConG <49092777+cccoding365@users.noreply.github.com>
2025-05-27 21:02:42 +0800

    Add random sentences

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // index.js
+const axios = require('axios');
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
+const url = 'https://v1.hitokoto.cn/?c=f&encode=text';
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+  'Accept': 'text/plain'
+};
+
 /**
   * DATA is the object that contains all
   * the data to be provided to Mustache
@@ -28,9 +35,10 @@ let DATA = {
   * C - We create a README.md file with the generated output
   */
 function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+  fs.readFile(MUSTACHE_MAIN_DIR, async (err, data) => {
     if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
+    const { data: text } = await axios.get(url, { headers });
+    const output = Mustache.render(data.toString(), { ...DATA, text });
     fs.writeFileSync('README.md', output);
   });
 }

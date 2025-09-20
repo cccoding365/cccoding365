7f168df0125be3dab1df23319f55d100903b1a5f
DoubledConG <992196871@qq.com>
2025-09-20 21:07:33 +0800

    feat: Add the weather information

--- index.js
+++ index.js
@@ -3,20 +3,10 @@ const axios = require('axios');
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-const url = 'https://v1.hitokoto.cn/?c=j&c=k&encode=json';
-const headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-  'Accept': 'text/plain'
-};
 
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
 let DATA = {
-  name: '小葱伴逗虎',
-  date: new Date().toLocaleDateString('zh-CN', {
+  name: 'DoubledConG',
+  date: new Date().toLocaleDateString('en-US', {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
@@ -29,17 +19,51 @@ let DATA = {
     timeZone: 'Asia/Shanghai'
   }),
 };
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, async (err, data) => {
+
+async function setHitokoto() {
+  const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept': 'text/plain'
+  };
+  const { data } = await axios.get('https://v1.hitokoto.cn/?c=d&c=i&c=k&encode=json', { headers });
+  DATA.hitokoto = data.hitokoto;
+  DATA.hitokoto_author = data.from_who;
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
-    const { data: { hitokoto: text, from_who: author } } = await axios.get(url, { headers });
-    const output = Mustache.render(data.toString(), { ...DATA, text, author });
+    const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
-generateReadMe();
+
+async function setWeatherInfo() {
+  const { data } = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=Amoy&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  );
+  DATA.city_temperature = Math.round(data.main.temp);
+  DATA.city_weather = data.weather[0].description;
+  DATA.city_weather_icon = data.weather[0].icon;
+  DATA.sun_rise = new Date(data.sys.sunrise * 1000).toLocaleString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Asia/Shanghai',
+  });
+  DATA.sun_set = new Date(data.sys.sunset * 1000).toLocaleString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Asia/Shanghai',
+  });
+}
+
+async function action() {
+
+  await setHitokoto();
+
+  await setWeatherInfo();
+
+  await generateReadMe();
+}
+
+action();
